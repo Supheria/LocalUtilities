c60c7e0da935b6ce71985cda991b6aff85bf0665
Supheria <Supheria@gmail.com>
2024-04-09 21:22:31 +0800

    - remove yaml serialization

--- FileUtilities/YamlFileSaverLoader.cs
+++ FileUtilities/YamlFileSaverLoader.cs
@@ -1,34 +1,34 @@
-﻿using YamlDotNet.Serialization;
+﻿//using YamlDotNet.Serialization;
 
-namespace LocalUtilities.FileUtilities;
+//namespace LocalUtilities.FileUtilities;
 
-public static class YamlFileSaverLoader
-{
-    public static void SaveToYaml<T>(this T obj, string path)
-    {
-        var streamWriter = File.CreateText(path);
-        var yamlSerializer = new Serializer();
-        yamlSerializer.Serialize(streamWriter, obj);
-        streamWriter.Close();
-    }
+//public static class YamlFileSaverLoader
+//{
+//    public static void SaveToYaml<T>(this T obj, string path)
+//    {
+//        var streamWriter = File.CreateText(path);
+//        var yamlSerializer = new Serializer();
+//        yamlSerializer.Serialize(streamWriter, obj);
+//        streamWriter.Close();
+//    }
 
-    public static string LoadFromYaml<T>(string path, out T? obj)
-    {
-        obj = default;
-        if (!File.Exists(path))
-            return $"{path} is not existed.";
-        var streamReader = File.OpenText(path);
-        try
-        {
-            var yamlDeserializer = new Deserializer();
-            obj = yamlDeserializer.Deserialize<T>(streamReader);
-            streamReader.Close();
-            return "";
-        }
-        catch (Exception e)
-        {
-            streamReader.Close();
-            return e.Message;
-        }
-    }
-}
+//    public static string LoadFromYaml<T>(string path, out T? obj)
+//    {
+//        obj = default;
+//        if (!File.Exists(path))
+//            return $"{path} is not existed.";
+//        var streamReader = File.OpenText(path);
+//        try
+//        {
+//            var yamlDeserializer = new Deserializer();
+//            obj = yamlDeserializer.Deserialize<T>(streamReader);
+//            streamReader.Close();
+//            return "";
+//        }
+//        catch (Exception e)
+//        {
+//            streamReader.Close();
+//            return e.Message;
+//        }
+//    }
+//}
