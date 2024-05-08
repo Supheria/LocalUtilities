1c1a0c7077e2da47ab0d0cc9f4a7870d6cb01780
Supheria <Supheria@gmail.com>
2024-05-09 01:23:14 +0800

    refine StringBuilder tool

--- StringUtilities/StringBuilderTool.cs
+++ StringUtilities/StringBuilderTool.cs
@@ -4,20 +4,25 @@ namespace LocalUtilities.StringUtilities;
 
 public static class StringBuilderTool
 {
-    public static StringBuilder AppendJoin<T>(this StringBuilder sb, char separator, IList<T> source, Func<StringBuilder, T, StringBuilder> func)
+    public static StringBuilder AppendJoin<T>(this StringBuilder sb, char separator, List<T> source, Action<StringBuilder, T> func)
     {
         if (source.Count is 0)
             return sb;
-
         if (source[0] is not null)
             func(sb, source[0]);
-
         for (var i = 1; i < source.Count; ++i)
         {
             sb.Append(separator);
             if (source[i] is not null)
                 func(sb, source[i]);
         }
+        return sb;
+    }
+
+    public static StringBuilder AppendTab(this StringBuilder sb, int times)
+    {
+        for (var i = 0; i < times; i++)
+            sb.Append('\t');
         return sb;
     }
 }
