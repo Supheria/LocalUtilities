eed1c41ab5009b52ce38af7a563250adda49e7de
Supheria <Supheria@gmail.com>
2024-05-27 21:16:38 +0800

    add operator to Coordinate

--- TypeGeneral/Coordinate.cs
+++ TypeGeneral/Coordinate.cs
@@ -38,6 +38,11 @@ public class Coordinate(int x, int y)
         return new((coordinate.X / factor).ToInt(), (coordinate.Y / factor).ToInt());
     }
 
+    public static Coordinate operator -(Coordinate left, Coordinate right)
+    {
+        return new(left.X - right.X, left.Y - right.Y);
+    }
+
     public override int GetHashCode()
     {
         return HashCode.Combine(X, Y);
