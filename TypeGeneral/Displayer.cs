98b5bd6e26e513c18bdd00ac820b014619a80153
Supheria <supheria@gmail.com>
2024-06-03 13:19:00 +0800

    remove shit in displayer

--- TypeGeneral/Displayer.cs
+++ TypeGeneral/Displayer.cs
@@ -1,13 +1,7 @@
-﻿
-
-namespace LocalUtilities.TypeGeneral;
+﻿namespace LocalUtilities.TypeGeneral;
 
 public abstract class Displayer : PictureBox
 {
-    protected Directions DragFlag { get; set; } = Directions.None;
-
-    protected Point DragStartPoint { get; set; }
-
     protected virtual FontData LabelFontData { get; set; } = new(nameof(LabelFontData))
     {
         Size = 17f,
@@ -15,39 +9,7 @@ public abstract class Displayer : PictureBox
 
     protected virtual FontData ContentFontData { get; set; } = new(nameof(ContentFontData));
 
-    public Displayer()
-    {
-        MouseDown += OnMouseDown;
-        AddOperations();
-        MouseMove += OnMouseMove;
-        MouseUp += OnMouseUp;
-    }
-
-    private void OnMouseDown(object? sender, MouseEventArgs e)
-    {
-        DragFlag = e.Button switch
-        {
-            MouseButtons.Left => Directions.Left,
-            MouseButtons.Right => Directions.Right,
-            MouseButtons.Middle => Directions.Center,
-            _ => Directions.None,
-        };
-        DragStartPoint = e.Location;
-    }
-
-    private void OnMouseUp(object? sender, MouseEventArgs e)
-    {
-        DragFlag = Directions.None;
-    }
-
-    protected abstract void AddOperations();
-
-    private void OnMouseMove(object? sender, MouseEventArgs e)
-    {
-        DragStartPoint = e.Location;
-    }
-
-    protected void Relocate()
+    public void Relocate()
     {
         if (Size == Image?.Size || Size.Width is 0 || Size.Height is 0)
             return;
